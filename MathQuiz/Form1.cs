dda7bd55a234728409563cdc03b821a91027271e
Brycenjwill <114171494+Brycenjwill@users.noreply.github.com>
2024-05-03 17:48:14 -0600

    updates

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -3,32 +3,22 @@ namespace MathQuiz
     public partial class Form1 : Form
     {
 
-        // Create a Random object called randomizer 
-        // to generate random numbers.
         Random randomizer = new Random();
 
-        // These integer variables store the numbers 
-        // for the addition problem. 
+
         int addend1;
         int addend2;
 
-        // These integer variables store the numbers 
-        // for the subtraction problem. 
+
         int minuend;
         int subtrahend;
-
-        // These integer variables store the numbers 
-        // for the multiplication problem. 
+ 
         int multiplicand;
         int multiplier;
 
-        // These integer variables store the numbers 
-        // for the division problem. 
         int dividend;
         int divisor;
 
-        // This integer variable keeps track of the 
-        // remaining time.
         int timeLeft;
 
         string date_ = DateTime.Now.ToString("d MMMM yyyy");
@@ -37,39 +27,30 @@ namespace MathQuiz
 
         public void StartTheQuiz()
         {
-            // Fill in the addition problem.
-            // Generate two random numbers to add.
-            // Store the values in the variables 'addend1' and 'addend2'.
+
             addend1 = randomizer.Next(51);
             addend2 = randomizer.Next(51);
 
-            // Convert the two randomly generated numbers
-            // into strings so that they can be displayed
-            // in the label controls.
+
             Console.Write("working up until add screen.");
             plusLeftLabel.Text = addend1.ToString();
             plusRightLabel.Text = addend2.ToString();
 
-            // 'sum' is the name of the NumericUpDown control.
-            // This step makes sure its value is zero before
-            // adding any values to it.
             sum.Value = 0;
 
-            // Fill in the subtraction problem.
             minuend = randomizer.Next(1, 101);
             subtrahend = randomizer.Next(1, minuend);
             minusLeftLabel.Text = minuend.ToString();
             minusRightLabel.Text = subtrahend.ToString();
             difference.Value = 0;
 
-            // Fill in the multiplication problem.
+
             multiplicand = randomizer.Next(2, 11);
             multiplier = randomizer.Next(2, 11);
             timesLeftLabel.Text = multiplicand.ToString();
             timesRightLabel.Text = multiplier.ToString();
             product.Value = 0;
 
-            // Fill in the division problem.
             divisor = randomizer.Next(2, 11);
             int temporaryQuotient = randomizer.Next(2, 11);
             dividend = divisor * temporaryQuotient;
@@ -77,10 +58,8 @@ namespace MathQuiz
             dividedRightLabel.Text = divisor.ToString();
             quotient.Value = 0;
 
-            //Set date value on quiz to display current date
             curDate.Text = "Date: " + date_;
 
-            // Start the timer.
             timeLeft = 30;
             timeLabel.Text = "30 seconds";
             timer1.Start();
@@ -139,9 +118,7 @@ namespace MathQuiz
         {
             if (CheckTheAnswer())
             {
-                // If CheckTheAnswer() returns true, then the user 
-                // got the answer right. Stop the timer  
-                // and show a MessageBox.
+
                 timer1.Stop();
                 MessageBox.Show("You got all the answers right!",
                                 "Congratulations!");
@@ -149,17 +126,13 @@ namespace MathQuiz
             }
             else if (timeLeft > 0)
             {
-                // If CheckTheAnswer() returns false, keep counting
-                // down. Decrease the time left by one second and 
-                // display the new time left by updating the 
-                // Time Left label.
+
                 timeLeft = timeLeft - 1;
                 timeLabel.Text = timeLeft + " seconds";
             }
             else
             {
-                // If the user ran out of time, stop the timer, show
-                // a MessageBox, and fill in the answers.
+
                 timer1.Stop();
                 timeLabel.Text = "Time's up!";
                 MessageBox.Show("You didn't finish in time.", "Sorry!");
@@ -173,7 +146,7 @@ namespace MathQuiz
 
         private void answer_Enter(object sender, EventArgs e)
         {
-            // Select the whole answer in the NumericUpDown control.
+
             NumericUpDown answerBox = sender as NumericUpDown;
 
             if (answerBox != null)
